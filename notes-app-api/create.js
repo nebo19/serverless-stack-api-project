9119b055e8292d5041e8d89791f931a745a86c2c
Nebojsa <nebojsa_pg@hotmail.com>
2020-02-04 12:26:29 +0100

    Removing console.log

--- notes-app-api/create.js
+++ notes-app-api/create.js
@@ -3,7 +3,6 @@ import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
-    console.log(event);
     const data = JSON.parse(event.body);
     const params = {
         TableName: process.env.tableName,
@@ -19,7 +18,6 @@ export async function main(event, context) {
         await dynamoDbLib.call("put", params);
         return success(params.Item);
     } catch (e) {
-        console.log(e);
         return failure({ status: false });
     }
 }

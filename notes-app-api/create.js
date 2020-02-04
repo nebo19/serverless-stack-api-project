8b5561d703fde8ade0f30f2d7681b23a1008465a
Nebojsa <nebojsa_pg@hotmail.com>
2020-02-04 12:09:10 +0100

    TableName change

--- notes-app-api/create.js
+++ notes-app-api/create.js
@@ -6,7 +6,7 @@ export async function main(event, context) {
     console.log(event);
     const data = JSON.parse(event.body);
     const params = {
-        TableName: "notes",
+        TableName: process.env.tableName,
         Item: {
             userId: event.requestContext.identity.cognitoIdentityId,
             noteId: uuid.v1(),
